2e9ec6203cd6bfa158b7edb1c885001cdaa2a0f4
Dev-Flores <brandon.giron.flores@gmail.com>
2022-03-08 15:17:39 -0300

    Initial commit

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,44 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String Titulo;
+    private String Descricao;
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "Titulo='" + Titulo + '\'' +
+                ", Descricao='" + Descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
